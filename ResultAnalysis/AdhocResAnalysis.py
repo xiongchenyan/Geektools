dd1fb523e94089cbeda540b706bcde8a32e39f0d
chenyanxiong <xiongchenyan@gmail.com>
2014-04-02 19:17:53 -0400

    res ana init

---
+++ ResultAnalysis/AdhocResAnalysis.py
@@ -0,0 +1,181 @@
+'''
+Created on Apr 2, 2014
+the base class for ad hoc result analysis
+input: output of ad hoc eva: each line a name\tadhocmeasurec.dump(), last line is mean
+    for baseline and all methods
+output:
+    a table, method * measure (will add win/loss/tie and percentage for required column)
+    a figure, showing per q win/lost bin compare with baseline
+@author: cx
+'''
+
+
+'''
+start with data read, conf set, and table first 4/2
+figure will call python's drawer, so only able to run on my window computer
+
+'''
+
+
+
+import site
+site.addsitedir('/bos/usr0/cx/PyCode/Geektools')
+site.addsitedir('/bos/usr0/cx/PyCode/cxPyLib')
+
+from AdhocEva.AdhocMeasure import *
+from ScoreBin import *
+from cxBase.base import *
+from copy import deepcopy
+
+class AdhocResAnalysisC(cxBaseC):
+    
+    def Init(self):
+        self.hBaseMeasure = {} #qid->measure
+        self.lhMethodMeasure = {} #dim [method]
+        self.lMethodName = []        
+        self.hMainMeasure = {'err':0} #method to focus
+        
+    def SetConf(self,ConfIn):
+        conf = cxConf(ConfIn)
+        lMethodName = conf.GetConf('methodname', [])
+        lMethodFName = conf.GetConf('methodevares',[])
+        for i in range(len(lMethodName)):
+            self.LoadEvaResForMethod(lMethodFName[i],lMethodName[i])
+        lMainMeasure = conf.GetConf('mainmeasure', 'err')
+        self.hMainMeasure = dict(zip(lMainMeasure,range(len(lMainMeasure))))
+        return True
+    
+    @staticmethod
+    def ShowConf():
+        print "methodname baseline#x\nmethodevares\nmainmeasure err"
+        
+    
+    
+    def LoadEvaResForMethod(self,ResFName,MeasureName):
+        lMeasure = ReadPerQEva(ResFName,True)
+        hMeasure = {}
+        for Measure in lMeasure:
+            hMeasure[Measure[0]] = Measure[1]
+        
+        if MeasureName == 'baseline':
+            self.hBaseMeasure = deepcopy(hMeasure)
+            return True
+        self.lMethodName.append(MeasureName)
+        self.lhMethodMeasure.append(deepcopy(hMeasure))
+        return True
+    
+    
+    @staticmethod
+    def RelativeGain(hBaseMeasure,hMeasure,MainMeasureName='err'):
+        if (not 'mean' in hBaseMeasure) | (not 'mean' in hMeasure):
+            print "mean value not calculate in calcing relative gain"
+            return -1
+        
+        Base = hBaseMeasure['mean'].GetMeasure(MainMeasureName)
+        Target = hMeasure['mean'].GetMeasure(MainMeasureName)
+        return AdhocResAnalysisC().CalcRelGain(Base,Target)
+    
+    @staticmethod
+    def CalcRelGain(Base,Target):
+        if 0 == Base:
+            return float(Target > 0)
+        RelGain = Target / Base - 1.0        
+        return RelGain
+    
+    @staticmethod
+    def WinLossTie(hBaseMeasure,hMeasure,MainMeasureName='err'):
+        Win = 0
+        Loss = 0
+        Tie = 0
+        
+        for item in hMeasure:
+            Target = hMeasure[item].GetMeasure(MainMeasureName)
+            Base = hBaseMeasure[item].GetMeasure(MainMeasureName)
+            if Target > Base:
+                Win += 1
+            if Target == Base:
+                Tie += 1
+            if Target < Base:
+                Loss += 1       
+        
+        return Win,Loss,Tie
+    
+    
+    @staticmethod
+    def WinLossNumBin(hBaseMeasure,hMeasure,MainMeasureName = 'err'):
+        #get the raw relative gain number
+        lRawRelGain = []
+        for item in hMeasure:
+            Base = hBaseMeasure[item].GetMeasure(MainMeasureName)
+            Target = hMeasure[item].GetMeasure(MainMeasureName)
+            lRawRelGain.append(AdhocResAnalysisC().CalcRelGain(Base, Target))
+            
+        #bin them to target bin
+        return BinValue(lRawRelGain)
+    
+    
+    
+    def FormResTable(self,caption='',label=''):
+        #form a latex format table
+        TableStr = "" #separated by \n for each row ^_^
+        
+        
+        TableStr += self.FormResTableHead(caption, label)
+        for i in range(len(self.lhMethodMeasure)):
+            hMeasure = self.lhMethodMeasure[i]
+            MethodName = self.lMethodName[i]
+            TableStr += self.FormTableRowForMethod(hMeasure,MethodName) 
+        TableStr += self.FormTableTail()
+        return TableStr 
+    
+    
+    def FormResTableHead(self,caption = "",label = 'tab:AdHocEva'):
+        
+        NumOfCol = AdhocMeasureC().NumOfMeasure() + len(self.hMainMeasure)*2 + 1
+        
+        TableHead = "\\begin{table}\\centering\\caption{%s\\label{%s}}" %(caption,label)
+        TableHead += "\\begin{tabular}{|%s}" %('c|'*NumOfCol)
+        TableHead += '\\hline\n'
+        
+        lMeasureName = AdhocMeasureC().MeasureName()
+        TableHead += 'method'
+        for MeasureName in lMeasureName:
+            TableHead += "& %s" %(MeasureName)
+            if MeasureName in self.hMainMeasure:
+                TableHead += "& relative Gain & Win/Loss/Tie"
+        TableHead += '\\\\ \\hline\n'           
+        return TableHead
+    
+    def FormTableRowForMethod(self,hMeasure,MethodName):
+        TableRow = "\\texttt{%s}" %(MethodName)
+        
+        for Measure in AdhocMeasureC().MeasureName():
+            score = hMeasure['mean'].GetMeasure(Measure)
+            TableRow += '&$%.3f$' %(score) 
+            if Measure in self.hMainMeasure:
+                if MethodName == 'baseline':
+                    TableRow += "&NA&NA"
+                else:
+                    RelGain = AdhocResAnalysisC().RelativeGain(self.hBaseMeasure,hMeasure,Measure)
+                    Win,Loss,Tie = AdhocResAnalysisC().WinLossNumBin(self.hBaseMeasure, hMeasure, Measure)
+                    TableRow +="&$%.0f%%$ &%d/%d/%d " %(100*RelGain, Win,Loss,Tie)
+        
+        TableRow += '\\\n'              
+        return TableRow
+    
+    def FormTableTail(self):
+        TableTail = "\end{tabular}\end{table}"
+        return TableTail
+    
+    
+    def DrawPerQGainFigure(self,FigOutName):
+        #draw a per q gain figure
+        return True
+    
+    
+    
+        
+        
+        
+
+
--- ResultAnalysis/ScoreBin.py
+++ ResultAnalysis/ScoreBin.py
@@ -31,13 +31,38 @@ def BinValue(l,n=20,BinSize = 0):
     if 0 == BinSize:
         BinSize = GetBinSize(l,n)
     hBin = {}
+        #add emtpy bin
+    for i in range(-(n/2+1), n/2 + 2):
+        if not i in hBin:
+            hBin[i] = 0   
     for value in l:
         BinNumber = GetBinNumber(value,BinSize)
         if math.fabs(BinNumber) > (n/2):
             BinNumber = (n/2+1) * math.fabs(BinNumber) /BinNumber            
-        if not BinNumber in hBin:
-            hBin[BinNumber] = 0
         hBin[BinNumber] += 1
+
     lBin = hBin.items()
     lBin.sort(key=itemgetter(0))
+    lBinRange = GenerateBinRange(n,BinSize)
+    for i in range(len(lBin)):
+        lBin[i][0] = lBinRange[i]
     return lBin
+
+
+def GenerateBinRange(n,BinSize):
+    lBinRange = [] #a string based bin range name
+    
+    lBinRange.append('(-\infty ,%.0f%%)' %(100.0* (-(n/2) * BinSize)))
+    
+    i = -(n/2)
+    while i < n/2:
+        if i == 0:
+            lBinRange.append('0')
+        else:
+            if i < 0:
+                lBinRange.append('[%f,%f)' %(100.0*(i * BinSize),100.0*((i+1)*BinSize)))
+            else:
+                lBinRange.append('(%f,%f]' %(100.0*(i * BinSize),100.0*((i+1)*BinSize)))        
+        i += 1
+    lBinRange.append('(%.0f%%,\infty)' %(100.0* ((n/2) * BinSize)))
+    return lBinRange
