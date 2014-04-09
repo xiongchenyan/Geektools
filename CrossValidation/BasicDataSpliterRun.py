b89da83bb15beadf9458a87635b6f14552eb31ee
chenyanxiong <xiongchenyan@gmail.com>
2014-04-08 21:46:59 -0400

    basic spliter

---
+++ CrossValidation/BasicDataSpliterRun.py
@@ -0,0 +1,43 @@
+'''
+Created on Apr 8, 2014
+
+@author: cx
+'''
+
+
+import site
+
+from RandomSplit import *
+from FoldNameGenerator import *
+class BasicDataSpliterC(DataSpliterC):
+    
+    def LoadData(self):
+        lData = [line.strip() for line in open(self.InName)]
+        return lData
+    
+    def OutData(self,lData,OutName):
+        out = open(OutName,'w')
+        for line in lData:
+            print >>out, line
+        out.close()
+        return True
+    
+    
+
+
+
+def BasicDataSpliterUnitRun(ConfIn):
+    BasicDataSpliterC.ShowConf()
+    Spliter = BasicDataSpliterC(ConfIn)
+    Spliter.Process()
+    return True
+
+
+import sys    
+     
+if 2 != len(sys.argv):
+    print "1conf:"
+    BasicDataSpliterC.ShowConf()
+    sys.exit()
+    
+BasicDataSpliterUnitRun(sys.argv[1])
