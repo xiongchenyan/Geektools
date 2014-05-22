47fdbdd29859ad5960e4743d84096053a296b504
chenyanxiong <xiongchenyan@gmail.com>
2014-05-22 17:33:48 -0400

    add output gzip

--- condor/DataKeySpliter.py
+++ condor/DataKeySpliter.py
@@ -13,6 +13,7 @@ from CrossValidation.FoldNameGenerator import *
 from cxBase.KeyFileReader import KeyFileReaderC
 import sys
 from cxBase.base import cxConf
+import gzip
 def ShowConf():
     print "in\nrootdir\nK\nkeyindex 0\ngzip false"
     
@@ -38,9 +39,13 @@ KeyReader.KeyIndex = KeyIndex
 KeyReader.UseGzip = UseGzip
 KeyReader.open(InName)
 
+print "cnting key"
 KeyCnt = 0
 for lvCol in KeyReader:
     KeyCnt += 1
+    if 0 == (KeyCnt % 10000):
+        print "cnt [%d] key" %(KeyCnt)
+    
     
 KeyReader.close()
 
@@ -49,13 +54,16 @@ print "total [%d] key" %(KeyCnt)
 N = int(math.ceil(float(KeyCnt) / K))
 print "each fold [%d] key" %(N)
 OutCnt = 0
-out = open(Namer.DataDir() + "/test_%d" %(OutCnt),'w')
+if UseGzip:
+    out = gzip.open(Namer.DataDir() + "/test_%d" %(OutCnt),'w')
+else:
+    out = open(Namer.DataDir() + "/test_%d" %(OutCnt),'w')
 
 cnt = 0
 
 
 KeyReader.open(InName)
-
+print "start spliting"
 for lvCol in KeyReader:
     for vCol in lvCol:
         print >> out,"\t".join(vCol)
